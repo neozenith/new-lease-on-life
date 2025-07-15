4ccacf1eb360046722118da135de30bcf7c7244e
Josh Peak <joshpeak05@gmail.com>
2025-07-15 12:38:18 +1000

    Improve extract polygons to not repeat work

--- extract_postcode_polygons.py
+++ extract_postcode_polygons.py
@@ -7,69 +7,123 @@
 # ]
 # ///
 
+import json
+import pathlib
+
 import geopandas as gpd
 import pandas as pd
-from shapely.ops import unary_union
-import pathlib
 
 # File paths
 POSTCODES_CSV = "postcodes.csv"
 STOPS_GEOJSON = "data/public_transport_stops.geojson"
 
 
-GEOJSON_PATH = "data/originals_converted/boundaries/POA_2021_AUST_GDA2020_SHP/POA_2021_AUST_GDA2020.geojson"
+POSTCODE_POLYGONS = (
+    "data/originals_converted/boundaries/POA_2021_AUST_GDA2020_SHP/POA_2021_AUST_GDA2020.geojson"
+)
 
 BOUNDARIES_BASE = pathlib.Path("data/originals_converted/boundaries")
 BOUNDARIES = BOUNDARIES_BASE.rglob("**/*.geojson")
 
 OUTPUT_ROOT = pathlib.Path("data/geojson/ptv/boundaries/")
 
+input_to_output_mapping = {
+    "postcodes": [POSTCODE_POLYGONS, POSTCODES_CSV],
+    "postcodes_with_trams": POSTCODE_POLYGONS,
+    "postcodes_with_trams_trains": POSTCODE_POLYGONS,
+}
+for b in BOUNDARIES:
+    output_target = b.stem.lower()
+    input_to_output_mapping[output_target] = b
+
+
+def check_output_up_to_date():
+    """
+    Check if the output files are up to date with respect to the input files.
+    """
+    files_to_process = {}
+    for target, input_file in input_to_output_mapping.items():
+        selected_output_file = (
+            OUTPUT_ROOT / f"selected_{target}.geojson"
+        )  # Selects subset of polygons
+        unioned_output_file = (
+            OUTPUT_ROOT / f"unioned_{target}.geojson"
+        )  # Unions the selected polygons
+
+        stops_mtime = pathlib.Path(STOPS_GEOJSON).stat().st_mtime
+        if isinstance(input_file, list):
+            input_file_max_mtime = max(
+                pathlib.Path(f).stat().st_mtime for f in input_file
+            )
+        else:
+            input_file_max_mtime = pathlib.Path(input_file).stat().st_mtime
+
+        input_file_max_mtime = max(input_file_max_mtime, stops_mtime)
+        
+        should_process = False
+        if (
+            not selected_output_file.exists()
+            or selected_output_file.stat().st_mtime < input_file_max_mtime
+        ):
+            should_process = True
+            files_to_process[target] = input_file
+
+        if (
+            not unioned_output_file.exists()
+            or unioned_output_file.stat().st_mtime < input_file_max_mtime
+        ):
+            should_process = True
+            files_to_process[target] = input_file
+
+        if should_process:
+            print(f"{target} needs processing.")
+
+    return files_to_process
+
+def filter_for_target(target, gdf_polygons, gdf_stops, code_col = None, code_list = None) -> gpd.GeoDataFrame:
+    """
+    Filter the GeoDataFrame of polygons based on the target and code list.
+    """
+    if target in ["postcodes"]:
+        return gdf_polygons[gdf_polygons[code_col].astype(str).isin(code_list)]
+    else:
+        return gpd.sjoin(gdf_polygons, gdf_stops, how="inner", predicate="intersects")
+    
+
+
 def extract_postcode_polygons():
     # Load the GeoJSON file
-    postcode_boundaries = gpd.read_file(GEOJSON_PATH)
+    work_to_do = check_output_up_to_date().items()
+    if len(work_to_do) == 0:
+        print("All outputs are up to date. No work to do.")
+        return
+    
+    postcode_boundaries = gpd.read_file(POSTCODE_POLYGONS)
 
     # Load postcodes from CSV (expects a column named 'postcode')
     postcodes = pd.read_csv(POSTCODES_CSV)
     postcode_list = postcodes.iloc[:, 1].astype(str).tolist()
-    print(f"{postcode_list=}")
 
     gdf_stops = gpd.read_file(STOPS_GEOJSON)
-    print(f"Loaded {gdf_stops['MODE'].unique()} modes from {STOPS_GEOJSON}")
-
     gdf_stops_trams_trains = gdf_stops[gdf_stops["MODE"].isin(["METRO TRAIN", "METRO TRAM"])].copy()
-    print(f"Filtered {len(gdf_stops_trams_trains)} stops for {gdf_stops_trams_trains['MODE'].unique()} modes.")
     gdf_stops_trams = gdf_stops[gdf_stops["MODE"].isin(["METRO TRAM"])].copy()
-    print(f"Filtered {len(gdf_stops_trams)} stops for {gdf_stops_trams['MODE'].unique()} modes.")
-
-
-    # The postcode column in the GeoJSON is usually 'POA_CODE21' or similar
-    postcode_col = None
-    for col in postcode_boundaries.columns:
-        if "POA" in col and "CODE" in col:
-            postcode_col = col
-            break
-    if postcode_col is None:
-        raise ValueError("Could not find postcode column in GeoJSON file.")
-    print(f"Using postcode column: {postcode_col}")
-
-
-
     
-    gdf_postcodes_with_trams_trains = gpd.sjoin(postcode_boundaries, gdf_stops_trams_trains, how="inner", predicate="intersects")
+    for target, input_file in work_to_do:
+        print(f"Processing target: {target} with input file: {input_file}")
+        if target == "postcodes":
+            gdf_polygons = filter_for_target(target, postcode_boundaries, gdf_stops, "POA_CODE21", postcode_list)
+        elif target == "postcodes_with_trams":
+            gdf_polygons = filter_for_target(target, postcode_boundaries, gdf_stops_trams)
+        elif target == "postcodes_with_trams_trains":
+            gdf_polygons = filter_for_target(target, postcode_boundaries, gdf_stops_trams_trains)
+        else:
+            # For other targets, load the specific boundary file
+            print(f"Loading input file: {input_file} {pathlib.Path(input_file).stat().st_size / 1024 / 1024:.2f} MB")
+            gdf_input = gpd.read_file(input_file)
+            gdf_polygons = filter_for_target(target, gdf_input, gdf_stops_trams_trains)
 
-
-    output_target_bases = {
-        # "postcodes": gdf_postcodes,
-        # "postcodes_with_trams": gdf_postcodes_with_trams,
-        "postcodes_with_trams_trains": gdf_postcodes_with_trams_trains
-    }
-
-    for b in BOUNDARIES:
-        b_gdf = gpd.read_file(b)
-        select__b_gdf = gpd.sjoin(b_gdf, gdf_stops_trams_trains, how="inner", predicate="intersects")
-        output_target_bases[b.stem.lower()] = select__b_gdf
-
-    for target, gdf in output_target_bases.items():
+        gdf = gdf_polygons
+    
         selected_output_file = OUTPUT_ROOT / f"selected_{target}.geojson"
         unioned_output_file = OUTPUT_ROOT / f"unioned_{target}.geojson"
 
@@ -77,15 +131,20 @@ def extract_postcode_polygons():
         unioned_output_file.parent.mkdir(parents=True, exist_ok=True)
 
         gdf.to_file(selected_output_file, driver="GeoJSON")
-        gdf.to_parquet(selected_output_file.with_suffix('.parquet'), engine='pyarrow', index=False)
-        print(f"Saved selected {target} to {selected_output_file}")
+        print(f"Saved selected {target} to {selected_output_file} {selected_output_file.stat().st_size / 1024 / 1024:.2f} MB")
+        gdf.to_parquet(selected_output_file.with_suffix(".parquet"), engine="pyarrow", index=False)
+        print(f"Saved selected {target} to {selected_output_file.with_suffix('.parquet')} {selected_output_file.with_suffix('.parquet').stat().st_size / 1024 / 1024:.2f} MB")
 
         unioned_geom = gdf.geometry.union_all()
-
         unioned_gdf = gpd.GeoDataFrame(geometry=[unioned_geom], crs=gdf.crs)
+
         unioned_gdf.to_file(unioned_output_file, driver="GeoJSON")
-        unioned_gdf.to_parquet(unioned_output_file.with_suffix('.parquet'), engine='pyarrow', index=False)
-        print(f"Saved unioned {target} to {unioned_output_file}")
+        print(f"Saved unioned {target} to {unioned_output_file}  {unioned_output_file.stat().st_size / 1024 / 1024:.2f} MB")
+        unioned_gdf.to_parquet(
+            unioned_output_file.with_suffix(".parquet"), engine="pyarrow", index=False
+        )
+        print(f"Saved unioned {target} to {unioned_output_file.with_suffix('.parquet')} {unioned_output_file.with_suffix('.parquet').stat().st_size / 1024 / 1024:.2f} MB")
+
 
 if __name__ == "__main__":
     extract_postcode_polygons()
